e4738d1bedc7b94b8575fc08c4875b9c1fe28e36
Grant Birkinbine <grantbirki@github.com>
2022-07-16 11:32:27 -0600

    lint

--- src/newrelic.js
+++ src/newrelic.js
@@ -4,22 +4,22 @@
  * See lib/config.defaults.js in the agent distribution for a more complete
  * description of configuration variables and their potential values.
  */
- exports.config = {
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['reactle-devops'],
+  /**
+   * Your New Relic license key.
+   * Make sureto set NEW_RELIC_LICENSE_KEY as this env variable
+   */
+  license_key: process.env.NEW_RELIC_LICENSE_KEY,
+  logging: {
     /**
-     * Array of application names.
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
      */
-    app_name : ['reactle-devops'],
-    /**
-     * Your New Relic license key.
-     * Make sureto set NEW_RELIC_LICENSE_KEY as this env variable
-     */
-    license_key : process.env.NEW_RELIC_LICENSE_KEY,
-    logging : {
-      /**
-       * Level at which to log. 'trace' is most useful to New Relic when diagnosing
-       * issues with the agent, 'info' and higher will impose the least overhead on
-       * production applications.
-       */
-      level : 'info'
-    }
-  };
+    level: 'info',
+  },
+}
